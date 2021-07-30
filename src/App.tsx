c1b0927ad4ae402b8ccaf6086dad16cef3aabae3
nishisuke <ryoskns+git@gmail.com>
2021-07-30 21:25:30 +0900

    bugnsa

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,19 @@ import {
   Link,
   RouteProps,
 } from "react-router-dom";
+import { ErrorBoundary } from "react-error-boundary";
+import Bugsnag from "@bugsnag/js";
+import BugsnagPluginReact from "@bugsnag/plugin-react";
+
 import { Realtime } from "pages/realtime";
 import { WebRtc } from "pages/web_rtc";
 import { CardBoard } from "pages/card_board";
-import { ErrorBoundary } from "react-error-boundary";
+import { ErrorCheck } from "pages/error_check";
+
+Bugsnag.start({
+  apiKey: "7af284a0a4ff4c45e09e4ff63d48ee46",
+  plugins: [new BugsnagPluginReact()],
+});
 
 const Error = () => <div>error</div>;
 const SkywayError = () => <div>Limit of skyway free plan</div>;
@@ -24,7 +33,9 @@ const RouteWithErrorBoundary: FC<RouteWrapProps> = ({
 }) => {
   return (
     <Route {...routeProps}>
-      <ErrorBoundary fallback={fallback}>{children}</ErrorBoundary>
+      <ErrorBoundary onError={(e) => Bugsnag.notify(e)} fallback={fallback}>
+        {children}
+      </ErrorBoundary>
     </Route>
   );
 };
@@ -47,6 +58,10 @@ function App() {
               </li>
             </ul>
           </nav>
+
+          <RouteWithErrorBoundary path="/error_check" fallback={<Error />}>
+            <ErrorCheck />
+          </RouteWithErrorBoundary>
 
           <RouteWithErrorBoundary path="/realtime" fallback={<Error />}>
             <Realtime />
---
+++ src/pages/error_check.tsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+
+export const ErrorCheck = () => {
+  const [renderError, setRenderError] = useState(false);
+  if (renderError) throw new Error("render error desu");
+
+  const rejectPromise = () => {
+    Promise.reject("promise reject desu");
+  };
+  const throwError = () => {
+    throw new Error("throw done desu");
+  };
+
+  return (
+    <>
+      <button onClick={() => setRenderError(true)}>
+        {renderError ? "to no error mode" : "to error mode"}
+      </button>
+
+      <button onClick={rejectPromise}>promise error</button>
+
+      <button onClick={throwError}>error in event handler</button>
+    </>
+  );
+};
